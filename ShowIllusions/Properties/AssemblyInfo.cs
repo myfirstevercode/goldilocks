61c4e9e7d70a72bc9e728a24247dbe4dbd46043f
myfirstevercode <cytodennis@gmail.com>
2015-10-15 02:45:58 +0800

    Update AssemblyInfo.cs

--- ShowIllusions/Properties/AssemblyInfo.cs
+++ ShowIllusions/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Show illusions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Added: Shadow and Shiva Effects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Show illusions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Ensage")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
