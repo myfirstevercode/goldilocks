f08add9a0a8503e6a4ed45ea83b8407a0143f5c3
myfirstevercode <cytodennis@gmail.com>
2015-10-16 10:35:00 +0800

    Create Program.cs

---
+++ GodofThunder/Program.cs
@@ -0,0 +1,14 @@
+namespace GodofThunder
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            GodofThunder.Init();
+        }
+
+        #endregion
+    }
+}
